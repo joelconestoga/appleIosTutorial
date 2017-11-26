c8c674da1b30ba74aaf1f1d10ad81a5147097b9f
Joel Koppen Matsu <joel@Joels-MacBook-Pro.local>
2017-11-26 17:25:47 -0500

    Created the Single View project

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Joel Koppen Matsu on 11/26/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
